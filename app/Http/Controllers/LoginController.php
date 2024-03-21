a64ead4d5c7bfb2d296c23c6aedb29ed6aa4dbcb
Izmi Nabilah Isnaini <izminabilah13@gmail.com>
2024-03-21 15:11:40 +0700

    add valiadation on login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\tu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use JetBrains\PhpStorm\NoReturn;
@@ -29,8 +29,33 @@ class LoginController extends Controller
      */
     public function store(Request $request)
     {
-//        var_dump($request);die();
-        return redirect()->route('home');
+        //validasi username requeire, password required
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        // get data tu berdasarkan username dan password
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $tu = tu::where('username', $username)->first();
+        if ($tu && $tu->password === $password) {
+            //check kalau ada di redirect ke halaman home
+            return redirect()->route('home');
+        } else {
+            //kalau ga ada di redirect lagi ke halaman login dengan error user not found
+            return redirect()->route('sign-in')->with('error', 'Invalid username or password, Masukkan kembali username dan password');
+        }
+
+
+
+
+
+//        if (Auth::attempt($credentials)) {
+//            return redirect()->route('home');
+//        }
+//        return redirect()->route('sign-in');
     }
 
     /**
--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'tu' => [
+            'driver' => 'session',
+            'provider' => 'tu',
+        ],
     ],
 
     /*
@@ -60,15 +64,18 @@ return [
     */
 
     'providers' => [
+
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+
+//         'users' => [
+//             'driver' => 'database',
+//             'table' => 'tu',
+//         ],
+
     ],
 
     /*
